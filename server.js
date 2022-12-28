d6864ef0a73abce031f408a07d9a67d5f1844397
HakobChalikyan <hchalikyan3@gmail.com>
2022-12-28 13:55:53 -0800

    heroku

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://HakobChalikyan:MongoPass@cluster0.pnd3gps.mongodb.net/AthletesDB")
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri)
+
+//mongoose.connect("mongodb+srv://HakobChalikyan:MongoPass@cluster0.pnd3gps.mongodb.net/AthletesDB")
 
 app.use("/", require("./routes/athleteRoute"));
 
-app.listen(3001, function() {
-    console.log("Express running on 3001");
-})
+app.listen(process.env.SERVER_PORT || 3001);
+
+// app.listen(3001, function() {
+//     console.log("Express running on 3001");
+// })
--- src/components/AthleteList.js
+++ src/components/AthleteList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, Component } from "react";
+import React, { useEffect, useState } from "react";
 
 function AthleteList() {
     const [athletes, setAthletes] = useState([{
